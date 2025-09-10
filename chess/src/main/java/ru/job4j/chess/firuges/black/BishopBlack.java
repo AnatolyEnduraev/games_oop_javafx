b70c93920863b0d65d3f79b5fab5adbcd2ae263e
Анатолий <eao73@mail.ru>
2025-09-10 20:31:37 +0300

    На проверку

---
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -0,0 +1,48 @@
+package ru.job4j.chess.firuges.black;
+
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+public class BishopBlack implements Figure {
+    private final Cell position;
+
+    public BishopBlack(final Cell ps) {
+        position = ps;
+    }
+
+    @Override
+    public Cell position() {
+        return position;
+    }
+
+    @Override
+    public Cell[] way(Cell dest) {
+        if (!isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(
+                    String.format("Could not move by diagonal from %s to %s", position, dest)
+            );
+        }
+        int deltaX = Integer.compare(dest.getX(), position.getX());
+        int deltaY = Integer.compare(dest.getY(), position.getY());
+        int size = Math.abs(dest.getX() - position.getX());
+        Cell[] steps = new Cell[size];
+        int x = position.getX() + deltaX;
+        int y = position.getY() + deltaY;
+        for (int index = 0; index < size; index++) {
+            steps[index] = Cell.findBy(x, y);
+            x += deltaX;
+            y += deltaY;
+        }
+        return steps;
+    }
+
+    public boolean isDiagonal(Cell source, Cell dest) {
+        return Math.abs(dest.getX() - source.getX()) == Math.abs(dest.getY() - source.getY());
+    }
+
+    @Override
+    public Figure copy(Cell dest) {
+        return new BishopBlack(dest);
+    }
+}
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BishopBlackTest {
+
+    @Test
+    void testPosition() {
+        BishopBlack bishop = new BishopBlack(Cell.C1);
+        Cell expected = Cell.C1;
+        assertThat(bishop.position()).isEqualTo(expected);
+    }
+
+    @Test
+    void testCopy() {
+        BishopBlack bishop = new BishopBlack(Cell.C1);
+        Figure copied = bishop.copy(Cell.D2);
+        assertThat(copied.position()).isEqualTo(Cell.D2);
+    }
+
+    @Test
+    void testWay() {
+        BishopBlack bishop = new BishopBlack(Cell.C1);
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertThat(bishop.way(Cell.G5)).isEqualTo(expected);
+    }
+
+    @Test
+    void testWayNotDiagonal() {
+        BishopBlack bishop = new BishopBlack(Cell.C1);
+        assertThatThrownBy(() -> bishop.way(Cell.C2))
+                .isInstanceOf(ImpossibleMoveException.class)
+                .hasMessageContaining("Could not move by diagonal");
+    }
+
+    @Test
+    void testIsDiagonalTrue() {
+        BishopBlack bishop = new BishopBlack(Cell.C1);
+        assertThat(bishop.isDiagonal(Cell.C1, Cell.G5)).isTrue();
+    }
+
+    @Test
+    void testIsDiagonalFalse() {
+        BishopBlack bishop = new BishopBlack(Cell.C1);
+        assertThat(bishop.isDiagonal(Cell.C1, Cell.C2)).isFalse();
+    }
+}
